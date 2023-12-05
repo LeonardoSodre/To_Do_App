d33d6527c1354ef91f5c1c9e6360e34b0bee639c
Leonardo Sodré <leonardo.f.sodre@gmail.com>
2023-12-05 10:33:40 -0300

    Iniciando a conexão com Mysql

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express")
 
 const exphbs = require("express-handlebars")
 
+const mysql = require("mysql2")
+
 const app = express()
 
 app.engine('handlebars', exphbs.engine())
@@ -14,7 +16,12 @@ app.get('/', (requisicao, resposta) =>{
     resposta.render('home')
 })
 
-app.listen(3000, () =>{
-    console.log('Servidor rodando na porta 3000')
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "todoapp",
+    port: 3306 //mudar 3307 no lab
 })
 
